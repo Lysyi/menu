46a12acf294c75f86fbcb5a78e4a053d09091c3b
Sasha <oleksandr.lysyi@gmail.com>
2015-12-08 14:12:56 +0200

    fixed js close button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,10 @@ $('.sf-menu > li').hover(function() {
   $(this).addClass('active');
   $(this).find('.submenu-container').stop(true, true).delay(200).fadeIn();
 }, function() {
-  $(this).find('.submenu-container').stop(true, true).delay(200).fadeOut();
   $(this).find('.close-submenu').click(function(){
     $('.submenu-container').stop(true, true).delay(100).fadeOut();
   });
+  $(this).find('.submenu-container').stop(true, true).delay(200).fadeOut();
   $(this).removeClass('active');
 });
 
