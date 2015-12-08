40c8854ffd8189300648313fb4f326507d65bf0e
Sasha <oleksandr.lysyi@gmail.com>
2015-12-08 10:14:01 +0200

    first commite

---
+++ app/scripts/main.js
@@ -0,0 +1,25 @@
+ /* global $*/
+'use strict';
+$('.sf-menu > li').hover(function() {
+  $(this).addClass('active');
+  $(this).find('.submenu-container').stop(true, true).delay(200).fadeIn();
+}, function() {
+  $(this).find('.submenu-container').stop(true, true).delay(200).fadeOut();
+  $(this).find('.close-submenu').click(function(){
+    $('.submenu-container').stop(true, true).delay(100).fadeOut();
+  });
+  $(this).removeClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
